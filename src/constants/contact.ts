3b6f2aed9b571ebf2fb90d1bf81fb496f8c365b0
luca <lgabrielcostafarias@gmail.com>
2026-04-11 13:42:42 -0300

    chore: mudanca de contato no botao de redirecionamento whatsapp

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,3 +1,3 @@
-export const WHATSAPP_NUMBER = "63991370849";
+export const WHATSAPP_NUMBER = "63992580388";
 export const WHATSAPP_MESSAGE = "Olá! Gostaria de falar com um técnico de TI.";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
